a9597d3ccf8cc2aa3652e8058e07a445d62d6c70
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-09-07 01:10:42 +0400

    Update and rename start_build.sh to build_script.sh

--- lineageos/build_script.sh
+++ lineageos/build_script.sh
@@ -7,7 +7,7 @@ export BUILD_USERNAME="nuruszama"
 export BUILD_HOSTNAME="creek"
 
 # Custom Build Tag
-export LINEAGE_BUILDTYPE="TFAS"
+export RELEASE_TYPE="RELEASE"
 
 # Build Optimizations & Checks
 export SKIP_ABI_CHECKS=true
@@ -25,7 +25,7 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs 
 git clone https://github.com/XiaomiCreek/android.git -b lineage-23.2 --depth=1 .repo/local_manifests
 
 # resync the repo source
-repo sync -j16 --force-sync
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # extract vendor tree
 curl -sfLo vendorextract.sh -z vendorextract.sh https://raw.githubusercontent.com/nuruszama/crave/creek/tools/vendorextract.sh
@@ -44,12 +44,21 @@ make installclean
 # start building
 mka bacon
 
+# Export environment variables for the upload script
+export SF_USER="nuruszama"
+export SF_PROJECT="xiaomicreek"
+export ANDROID_VER="16"
+export ROM_NAME="LineageOS"
+
+# Custom SSH key location (if using a different key or path)
+export SSH_KEY="$HOME/.ssh/id_ed25519"
+
 # Upload
-echo "upload to gofile..."
+echo "uploading file..."
 ROM_DIR="out/target/product/creek/"
-ZIP_FILE=$(ls "$ROM_DIR" | grep "lineage-.*-creek.zip$" | tail -n 1)
+ZIP_FILE=$(ls "$ROM_DIR" | grep "lineage-creek-*.zip$" | tail -n 1)
 if [ -n "${ZIP_FILE}" ]; then
-    curl -sfLo upload.sh -z upload.sh https://raw.githubusercontent.com/nuruszama/crave/creek/tools/GoFile-upload.sh
+    curl -sfLo upload.sh -z upload.sh https://raw.githubusercontent.com/nuruszama/crave/creek/tools/sf-upload.sh
     chmod +x upload.sh ; ./upload.sh "${ROM_DIR}${ZIP_FILE}"
     echo "upload done!"
 else
