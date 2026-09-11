f23adc966e631fef636650d0335fd408c42f2868
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-09-11 08:59:34 +0400

    Update build_script.sh

--- lineageos/build_script.sh
+++ lineageos/build_script.sh
@@ -112,10 +112,10 @@ for VARIANT in "${VARIANTS[@]}"; do
     export BUILD_HOSTNAME=creek
 
     # Custom Build Tag
-    export RELEASE_TYPE=stable
+    export RELEASE_TYPE=RELEASE
 
     # Make build name unique (e.g. erofs-vanilla, ext4-vanilla, erofs-gapps)
-    export LINEAGE_BUILDTYPE="${FS_TYPE}-${GAPPS_CHOICE}"
+    export LINEAGE_BUILD="${FS_TYPE}-${GAPPS_CHOICE}"
     
     # prepare device menu
     breakfast creek userdebug
